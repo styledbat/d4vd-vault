0917b5a72751d5a9a11a3b0d54b09aff2359cd38
styledbat <styledisafatbat@gmail.com>
2026-08-11 12:07:56 +0100

    Update albums.js

--- albums.js
+++ albums.js
@@ -1,36 +1,22 @@
 const albums = [
+
     {
-        id: "decay-ep",
         name: "Decay EP",
+
         type: "EP",
+
         folder: "decay-ep",
+
         cover: "cover.png",
 
         tracks: [
-            {
-                title: "Issues",
-                file: "01-issues.mp3"
-            },
-            {
-                title: "How I See You",
-                file: "02-how-i-see-you.mp3"
-            },
-            {
-                title: "Take The Photos",
-                file: "03-take-the-photos.mp3"
-            },
-            {
-                title: "Summer Don't End",
-                file: "04-summer-dont-end.mp3"
-            },
-            {
-                title: "Empty The Clip",
-                file: "05-empty-the-clip.mp3"
-            },
-            {
-                title: "I'm In Love",
-                file: "06-im-in-love.mp3"
-            }
+            "01-issues.mp3",
+            "02-how-i-see-you.mp3",
+            "03-take-the-photos.mp3",
+            "04-summer-dont-end.mp3",
+            "05-empty-the-clip.mp3",
+            "06-im-in-love.mp3"
         ]
     }
+
 ];
