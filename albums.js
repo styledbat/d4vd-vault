671f1d9cc3154fab752c12d45dbb0073dba2c67b
styledbat <styledisafatbat@gmail.com>
2026-08-11 12:06:19 +0100

    Update albums.js

--- albums.js
+++ albums.js
@@ -1,70 +1,36 @@
-/*
-    d4vd.vault
-    Album database
-
-    Folder structure:
-
-    albums/
-    └── decay-ep/
-        ├── cover.png
-        ├── 01-issues.mp3
-        ├── 02-how-i-see-you.mp3
-        ├── 03-take-the-photos.mp3
-        ├── 04-summer-dont-end.mp3
-        ├── 05-empty-the-clip.mp3
-        └── 06-im-in-love.mp3
-*/
-
 const albums = [
-
     {
         id: "decay-ep",
-
-        name: "Decay",
-
+        name: "Decay EP",
         type: "EP",
-
         folder: "decay-ep",
-
         cover: "cover.png",
 
         tracks: [
             {
                 title: "Issues",
-                file: "01-issues.mp3",
-                status: "EP"
+                file: "01-issues.mp3"
             },
-
             {
                 title: "How I See You",
-                file: "02-how-i-see-you.mp3",
-                status: "EP"
+                file: "02-how-i-see-you.mp3"
             },
-
             {
                 title: "Take The Photos",
-                file: "03-take-the-photos.mp3",
-                status: "EP"
+                file: "03-take-the-photos.mp3"
             },
-
             {
                 title: "Summer Don't End",
-                file: "04-summer-dont-end.mp3",
-                status: "EP"
+                file: "04-summer-dont-end.mp3"
             },
-
             {
                 title: "Empty The Clip",
-                file: "05-empty-the-clip.mp3",
-                status: "EP"
+                file: "05-empty-the-clip.mp3"
             },
-
             {
                 title: "I'm In Love",
-                file: "06-im-in-love.mp3",
-                status: "EP"
+                file: "06-im-in-love.mp3"
             }
         ]
     }
-
 ];
